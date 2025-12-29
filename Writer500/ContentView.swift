2de58a74334fd4f823796e9da8092f9b220daf7e
Matt Gay <matthew.eric.gay@gmail.com>
2025-12-29 14:32:37 -0500

    Initial Commit

---
+++ Writer500/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Writer500
+//
+//  Created by Matt Gay on 12/29/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Writer500/Writer500App.swift
@@ -0,0 +1,17 @@
+//
+//  Writer500App.swift
+//  Writer500
+//
+//  Created by Matt Gay on 12/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Writer500App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
